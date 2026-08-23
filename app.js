75c8e8f07491ed282a3b75e320d2b1665a84f170
kattief5640-afk <kattief5640@gmail.com>
2026-08-23 15:37:40 -0400

    Add feeling input and alert on continue button click

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ beginButton.addEventListener("click", function () {
   checkin.style.display = "block";
 });
 
+const continueButton = document.querySelector("#continueButton");
+const feelingInput = document.querySelector("#feelingInput");
+
+continueButton.addEventListener("click", function () {
+  const feeling = feelingInput.value;
+
+  alert("Thank you for sharing. You said: " + feeling);
+});
