6973dbc7b7c9d0f2b670a15fafddee1ab23d6060
kattief5640-afk <kattief5640@gmail.com>
2026-08-23 15:31:19 -0400

    Show check-in element on button click

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const beginButton = document.querySelector("button");
+const checkin = document.querySelector("#checkin");
 
 beginButton.addEventListener("click", function () {
-  alert("I'm glad you're here. You can take your time. Tell me how you're really doing today.");
+  checkin.style.display = "block";
 });
+
