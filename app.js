328ddf25433d7bb9878ffc755b841275a5ccbe85
kattief5640-afk <kattief5640@gmail.com>
2026-08-23 14:06:41 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const beginButton = document.querySelector("button");
+
+beginButton.addEventListener("click", function () {
+  alert("I'm glad you're here. You can take your time. Tell me how you're really doing today.");
+});
